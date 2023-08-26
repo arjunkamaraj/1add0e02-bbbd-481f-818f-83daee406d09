5b79e97d0cefcd3b961063ddb5a30807aa767782
arjun-kamaraj <Arjun.Kamaraj@kmart.com.au>
2023-08-26 20:56:10 +1000

    commit

---
+++ Longest_Increasing_Subsequence/Longest_Increasing_Subsequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Increasing_Subsequence
+{
+    public class LongestIncreasingSubsequence
+    {
+        /// <summary>
+        /// Find Longest Increasing Subsequence
+        /// </summary>
+        /// <param name="input">Sequence of integers seperated by spaces </param>
+        /// <returns>Longest increasing subsequence present in input sequence</returns>
+        public static string FindLongestIncreasingSubsequence(string input)
+        {
+            // Split the input string into an array of integers    
+            var numbers = input.Split(' ').Select(int.Parse).ToList();
+
+            int maxLength = int.MinValue, i = 0, j = 1, startindex = 0, prevStartIndex = 0, difference = 0;
+
+            while (i < numbers.Count && j < numbers.Count)
+            {
+                if (numbers[i] < numbers[j])
+                {
+                    i++;
+
+                    if (j == numbers.Count - 1)
+                    {
+                        // when last digit is greater than the number[i], we have to add 1 more.
+                        j++;
+                        difference = (j - startindex);
+                        if (maxLength < difference)
+                        {
+                            prevStartIndex = startindex;
+                            maxLength = difference;
+                        }
+                    }
+                }
+                else
+                {
+                    difference = (j - startindex);
+                    if (maxLength < (difference))
+                    {
+                        prevStartIndex = startindex;
+                        maxLength = difference;
+                    }
+                    i = j;
+                    startindex = i;
+                }
+
+                j++;
+            }
+
+            var list = numbers.Skip(prevStartIndex).Take(maxLength).ToList();
+            return string.Join(' ', list);
+
+        }
+    }
+}
---
+++ Longest_Increasing_Subsequence/Longest_Increasing_Subsequence/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Longest_Increasing_Subsequence;
+
+do
+{
+    // Increasing size of inputBuffer array if the Input size is more than 65534 characters( 2 bytes are reserved).
+    byte[] inputBuffer = new byte[65536];
+    Stream inputStream = Console.OpenStandardInput(inputBuffer.Length);
+    Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
+
+    Console.WriteLine("\nEnter a sequence of integers separated by spaces (Max Characters -: 65534)");
+    string input = Console.ReadLine().Trim();
+
+    bool invalidInput = input.Any(x => x != ' ' && !char.IsDigit(x));
+    if (invalidInput)
+    {
+        Console.WriteLine("\nInvalid input!, please enter only integers by spaces");        
+    }
+    else
+    {
+        string longestIncreasingSubsequence = LongestIncreasingSubsequence.FindLongestIncreasingSubsequence(input);
+        Console.WriteLine("\n\n----------------------------------------------------------------------------------------------------------");
+        Console.WriteLine($"Longest Increasing Subsequence: {longestIncreasingSubsequence}");
+    }
+    Console.WriteLine("\n\n----------------------------------------------------------------------------------------------------------");
+    Console.WriteLine("Do you want to continue (Y/N)? ");
+}
+while (Console.ReadKey().Key == ConsoleKey.Y);
